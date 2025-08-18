435ef9c94db61b4f074822536969eb0431009d36
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-08-18 17:26:06 +0500

    minor

--- app/Http/Controllers/Customer/RideController.php
+++ app/Http/Controllers/Customer/RideController.php
@@ -185,7 +185,7 @@ class RideController extends Controller
 
             // 5: notify the nearby riders
 
-            $data  = dispatch(new SearchNearbyRidersJob($ride->id));
+            SearchNearbyRidersJob::dispatch($ride->id);
             // $data = notifyNearbyRiders($ride->vehicleTypeRateId, $ride->lat, $ride->lng, 1, 10, $ride);
 
             // return $data;

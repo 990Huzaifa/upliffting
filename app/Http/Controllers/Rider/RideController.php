1793ba76bf00139a85765c629e0c698fe975adc9
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-09-02 19:35:02 +0500

    changes update

--- app/Http/Controllers/Rider/RideController.php
+++ app/Http/Controllers/Rider/RideController.php
@@ -15,23 +15,38 @@ use Illuminate\Support\Facades\Auth;
 
 class RideController extends Controller
 {
-    public function acceptRide(string $id): JsonResponse
+    public function acceptRide(Request $request,string $id): JsonResponse
     {
         try{
             $user = Auth::user();
             $ride = Rides::find($id);
-            if($ride->status != 'finding') throw new Exception('Ride not available', 400);
-            $vehicle_id = Vehicle::where('vehicle_of',$user->id)->where('is_driving','active')->value('id');
-            if(!$vehicle_id) throw new Exception('You have no active vehicle', 400);
+
+
+            // here we manage cases
+            $status = $request->input('status');
+            switch($status){
+                case 'on a way':
+                    if($ride->status != 'finding') throw new Exception('Ride not available', 400);
+                    break;
+                case 'arrived':
+                    if($ride->status != 'on a way') throw new Exception('Ride not available', 400);
+                    break;
+                default:
+                    throw new Exception('Invalid status', 400);
+            }
+
+            
+            $vehicle = Vehicle::where('vehicle_of',$user->id)->where('is_driving','active')->first();
+            if(!$vehicle) throw new Exception('You have no active vehicle', 400);
+            // if($vehicle->approved_at == null) throw new Exception('Your vehicle is not approved', 400);
             
             // update ride data 
             $ride->update([
                 'rider_id' => $user->id,
                 'status' => 'on a way',
-                'vehicle_id' => $vehicle_id,
+                'vehicle_id' => $vehicle->id,
             ]);
 
-            // HandleRiderResponseJob::dispatch($ride->id, $user->id, 'accept');
             // fire event
             $title = 'Driver Found!';
             $data = [
@@ -39,9 +54,18 @@ class RideController extends Controller
                 'status' => 'on a way',
                 'riderId' => $user->id,
                 'riderName' => $user->first_name . ' ' . $user->last_name,
+                'riderAvatar' => $user->avatar,
                 'riderPhone' => $user->phone,
                 'riderLat' => $user->lat,
-                'riderLng' => $user->lng
+                'riderLng' => $user->lng,
+                'vehicleInfo' => [
+                    'id' => $vehicle->id,
+                    'make' => $vehicle->make,
+                    'model' => $vehicle->model,
+                    'color' => $vehicle->color,
+                    'plate_number' => $vehicle->registration_number,
+                    'photos' => $vehicle->photos,
+                ],
             ];
             broadcast(new RideAccepted(
                     $title,

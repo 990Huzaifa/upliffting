fa9b4e9c0e30fc2377853242de0cec01cbaf911f
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-11-03 14:37:01 +0500

    teset case

--- app/Http/Controllers/Customer/RideController.php
+++ app/Http/Controllers/Customer/RideController.php
@@ -399,7 +399,7 @@ class RideController extends Controller
                 throw new Exception('You are not authorized to rate this ride.', 403);
             }
 
-            if ($ride->status !== 'completed') {
+            if ($ride->status !== 'end trip') {
                 throw new Exception('You can only rate a completed ride.', 400);
             }
 

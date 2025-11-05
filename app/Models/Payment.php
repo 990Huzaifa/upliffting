bf82f7e8d1a602938fb5c4d2ac9d87e5e67d010b
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-11-05 11:03:48 +0500

    add tip in payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -14,6 +14,7 @@ class Payment extends Model
         'customer_id',
         'rider_id',
         'amount',
+        'tip',
         'payment_method_id',
         'transaction_id',
         'status',

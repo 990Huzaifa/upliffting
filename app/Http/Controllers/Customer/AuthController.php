1d79689180842e9022f8f9632e2dbc6f4df4f37e
990Huzaifa <syedhuzaifaali366@gmail.com>
2025-11-05 16:58:23 +0500

    test case 2 empty

--- app/Http/Controllers/Customer/AuthController.php
+++ app/Http/Controllers/Customer/AuthController.php
@@ -534,7 +534,7 @@ class AuthController extends Controller
 
             return response()->json([
                 'message' => 'Location updated successfully',
-                'nearby_riders' => $merged
+                'nearby_riders' => []
             ], 200);   
         }catch(QueryException $e){
             return response()->json(['DB error' => $e->getMessage()], 500);
